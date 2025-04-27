93ff4f860a7fd1ead05c0b091415177d9bf3b4dc
Samson Lasode <samflicks@gmail.com>
2025-04-27 14:49:59 +0100

    Updated autoscale with proper CPU-based scaling rules

--- autoscale.tf
+++ autoscale.tf
@@ -1,47 +1,57 @@
 # autoscale.tf
-resource "azurerm_public_ip" "main" {
-  name                = "${local.resource_name}-lb-pip"
+resource "azurerm_monitor_autoscale_setting" "main" {
+  name                = "${var.resource_prefix}-${var.environment}-autoscale"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-  tags                = local.common_tags
-}
+  target_resource_id  = azurerm_orchestrated_virtual_machine_scale_set.main.id
 
-resource "azurerm_lb" "main" {
-  name                = "${local.resource_name}-lb"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  sku                 = "Standard"
-  tags                = local.common_tags
+  profile {
+    name = "default"
 
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.main.id
+    capacity {
+      default = 2
+      minimum = 2
+      maximum = 5
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_orchestrated_virtual_machine_scale_set.main.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 80
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_orchestrated_virtual_machine_scale_set.main.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 10
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
   }
-}
-
-resource "azurerm_lb_backend_address_pool" "main" {
-  loadbalancer_id = azurerm_lb.main.id
-  name            = "BackEndAddressPool"
-}
-
-resource "azurerm_lb_probe" "main" {
-  loadbalancer_id     = azurerm_lb.main.id
-  name                = "http-probe"
-  port                = 80
-  protocol            = "Tcp"
-  interval_in_seconds = 5
-  number_of_probes    = 2
-}
-
-resource "azurerm_lb_rule" "main" {
-  loadbalancer_id                = azurerm_lb.main.id
-  name                           = "http"
-  protocol                       = "Tcp"
-  frontend_port                  = 80
-  backend_port                   = 80
-  frontend_ip_configuration_name = "PublicIPAddress"
-  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.main.id]
-  probe_id                       = azurerm_lb_probe.main.id
 }
