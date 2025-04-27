ae0000eebc785d298e1bb18fcd2b877951930d65
Samson Lasode <samflicks@gmail.com>
2025-04-27 14:50:54 +0100

    Added dedicated load balancer configuration with proper naming

---
+++ lb.tf
@@ -0,0 +1,45 @@
+# lb.tf (new file)
+resource "azurerm_public_ip" "lb" {
+  name                = "${var.resource_prefix}-${var.environment}-lb-pip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  domain_name_label   = "${var.resource_prefix}-${var.environment}-${random_pet.lb_hostname.id}"
+}
+
+resource "azurerm_lb" "main" {
+  name                = "${var.resource_prefix}-${var.environment}-lb"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.lb.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  name            = "${var.resource_prefix}-${var.environment}-backend-pool"
+  loadbalancer_id = azurerm_lb.main.id
+}
+
+resource "azurerm_lb_probe" "main" {
+  name            = "${var.resource_prefix}-${var.environment}-http-probe"
+  loadbalancer_id = azurerm_lb.main.id
+  protocol        = "Http"
+  port            = 80
+  request_path    = "/"
+}
+
+resource "azurerm_lb_rule" "main" {
+  name                           = "${var.resource_prefix}-${var.environment}-http"
+  loadbalancer_id                = azurerm_lb.main.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "PublicIPAddress"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.main.id]
+  probe_id                       = azurerm_lb_probe.main.id
+}
