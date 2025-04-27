c701547ecaeb52cf34cb6b20453758b014e7d3af
Samson Lasode <samflicks@gmail.com>
2025-04-27 14:38:15 +0100

    Added load balancer configuration with public IP and health probe

---
+++ autoscale.tf
@@ -0,0 +1,47 @@
+# autoscale.tf
+resource "azurerm_public_ip" "main" {
+  name                = "${local.resource_name}-lb-pip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = local.common_tags
+}
+
+resource "azurerm_lb" "main" {
+  name                = "${local.resource_name}-lb"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "Standard"
+  tags                = local.common_tags
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  loadbalancer_id = azurerm_lb.main.id
+  name            = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_probe" "main" {
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "http-probe"
+  port                = 80
+  protocol            = "Tcp"
+  interval_in_seconds = 5
+  number_of_probes    = 2
+}
+
+resource "azurerm_lb_rule" "main" {
+  loadbalancer_id                = azurerm_lb.main.id
+  name                           = "http"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "PublicIPAddress"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.main.id]
+  probe_id                       = azurerm_lb_probe.main.id
+}
